6f8d99bda8e633801dd9c034df6acb3e057ef726
Adriano Ribeiro Franulovic Campos <a173253@dac.unicamp.br>
2024-05-07 16:05:24 -0300

    Modificar Classe Ataque
    -Mudar visibilidade das propriedades de "private" para "protected"
    -Criar propriedade "ppMax"
    -Documentar propriedades e métodos

--- pokemon/Ataque.java
+++ pokemon/Ataque.java
@@ -1,12 +1,43 @@
 package pokemon;
 
 public abstract class Ataque {
-    private Tipo tipo;
-    private int poder;
-    private int pp;
-    private int prioridade;
-    private int precisao;
+    /**
+     * O tipo do ataque.
+     */
+    protected Tipo tipo;
+    /**
+     * O poder (força) do ataque.
+     */
+    protected int poder;
+    /**
+     * O número de PPs disponíveis (pontos de poder,
+     * que limitam quantas vezes é possível usar o ataque).
+     */
+    protected int pp;
+    /**
+     * O máximo de PPs que o ataque pode ter.
+     */
+    protected int ppMax;
+    /**
+     * A prioridade do ataque.
+     * Ataques com prioridades mais altas acontecem primeiro.
+     */
+    protected int prioridade;
+    /**
+     * A probabilidade (em porcentagem) de o ataque
+     * acertar o alvo, desprezando evasão e outros efeitos.
+     */
+    protected int precisao;
 
+    /**
+     * Calcula o dano do ataque, caso ele acerte.
+     *
+     * @param usuario o Pokémon usando o ataque
+     * @param alvo    o Pokémon alvo
+     * @param clima   o clima atual
+     * @return o dano.
+     */
     public abstract int dano(Pokemon usuario, Pokemon alvo, Clima clima);
+
     public abstract Efeito efeito();
 }
