7e0529bfa09c2497592d23b921f7664137e43fb7
Pedro Silva <p200246@dac.unicamp.br>
2024-05-14 14:56:23 -0300

    Commit dos itens parte nao sei

---
+++ pokemon/I_X.java
@@ -0,0 +1,10 @@
+package pokemon;
+/*
+ * Classe que determina todos os itens do formato item de batalha da forma X.
+ * Esses itens consomem um turno e aumentam um certo status do pokemon pela duração
+ * da batalha. Esse bônus é zerado quando o pokemon morre e está sujeito a alterações
+ * caso ele sofra algum de ataque de status ou inflinga alguma punição de status em si mesmo.
+ */
+public class I_X {
+    
+}
