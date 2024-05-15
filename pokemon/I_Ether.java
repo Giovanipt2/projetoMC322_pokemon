964378534177f379195fb3f80d5ce3db3d3fa3fd
Pedro Silva <p200246@dac.unicamp.br>
2024-05-15 17:11:17 -0300

    Começando os Ethers e Elixirs

--- pokemon/I_Ether.java
+++ pokemon/I_Ether.java
@@ -1,5 +1,19 @@
 package pokemon;
 
-public class I_Ether {
-    
+
+/*
+ * Classe de itens destinada a curar o PP de um ataque do pokemon em questão.
+ * Só pode ser usada em pokemons vivos e em um ataque que não esteja no seu valor
+ * máximo de PP.
+ */
+public class I_Ether extends ItemBatalha{
+    private boolean isMax; // Booleano novamente para facilitar a implementação
+
+
+    // Construtor
+    public I_Ether(String nome, boolean isMax) {
+        super(nome);
+        this.isMax = isMax;
+    }
+
 }
